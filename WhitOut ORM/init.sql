9dae4b6eed865021499ccb725b57d5cc7ffb9b71
Camiloballen24-02 <camiloballen24@gmail.com>
2020-08-04 15:03:55 -0500

    :rocket: Prueba Sin ORM

--- WhitOut ORM/init.sql
+++ WhitOut ORM/init.sql
@@ -4,7 +4,7 @@ GRANT ALL PRIVILEGES ON DATABASE super_market_db TO postgres;
 CREATE TABLE public.categories
 (
     id integer NOT NULL DEFAULT nextval('categories_id_seq'::regclass),
-    name character varchar(30) NOT NULL,
+    name varchar(30) NOT NULL,
     CONSTRAINT categories_pkey PRIMARY KEY (id)
 );
 
