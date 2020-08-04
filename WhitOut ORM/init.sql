1258072967087f8d0f3c56c127a757b9c59d12f7
Camiloballen24-02 <camiloballen24@gmail.com>
2020-08-04 15:19:09 -0500

    :rocket: Prueba Sin ORM

--- WhitOut ORM/init.sql
+++ WhitOut ORM/init.sql
@@ -12,3 +12,20 @@ CREATE TABLE public.categories
 
 ALTER TABLE public.categories
     OWNER to postgres;
+
+CREATE TABLE public.products
+(
+    id integer NOT NULL,
+    name varchar(50) NOT NULL,
+    quantityinpackage smallint NOT NULL,
+    unitofmeasurement smallint NOT NULL,
+    categoryid integer NOT NULL,
+    CONSTRAINT products_pkey PRIMARY KEY (id),
+    CONSTRAINT fk_category FOREIGN KEY (categoryid)
+        REFERENCES public.categories (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE CASCADE
+)
+
+ALTER TABLE public.products
+    OWNER to postgres;
